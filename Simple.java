9d14dbdc04e1a0bdc80e8665207fae6d579111d6
DhanaDurga <116088645+DhanaDurga@users.noreply.github.com>
2023-09-19 06:14:21 +0530

    Simple Java program

---
+++ Simple.java
@@ -0,0 +1,13 @@
+class Simple{  
+
+	public String call(){
+ 
+    return "Welcome to TGI";
+}
+
+    static void main(String args[]){ 
+Simple obj=new Simple();
+String res=obj.call();
+System.out.println(res);
+    }  
+} 
